1c75fb9972bce189b39bfaf9d53679aaf25b5382
Brenda <bren.itzel14@gmail.com>
2023-11-29 20:27:22 -0600

    first commit

---
+++ src/views/TabsPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+
+        <ion-tab-button tab="tab1" href="/tabs/tab1">
+          <ion-icon :icon="square" />
+          <ion-label>welcome</ion-label>
+        </ion-tab-button>
+        
+        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-icon :icon="images" />
+        <ion-label>Photos</ion-label>
+        </ion-tab-button>
+
+
+
+        <ion-tab-button tab="tab3" href="/tabs/tab3">
+          <ion-icon :icon="square" />
+          <ion-label>Thanks</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { images, square, triangle } from 'ionicons/icons';
+</script>
